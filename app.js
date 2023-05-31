b2224999530b44f960ed602dcb12d5a1b38a7e7f
Aabid Majid <aabidmajid26@gmail.com>
2023-05-31 10:34:29 +0530

    small chaage

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const imageContainer = document.getElementById('image-container');
 const API_KEY = "fjBWTdYh5E38CxFPr8zyo69niO8h5jwjnrgBfPiH"
 
 // Fetch NASA API
-fetch('https://api.nasa.gov/planetary/apod?api_key=${API_KEY}')
+fetch('https://api.nasa.gov/planetary/apod?api_key=fjBWTdYh5E38CxFPr8zyo69niO8h5jwjnrgBfPiH')
   .then(response => response.json())
   .then(data => {
     // Update the image container with the fetched image
